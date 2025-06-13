8dfd965e2300717141fd3a205899feb2127989db
Carlos Antônio Zarzar <55281550+carloszarzar@users.noreply.github.com>
2025-06-12 21:11:53 -0300

    Update desafio.py
    Modificação de output de feedback para o usuário.

--- 00 - Fundamentos/desafio.py
+++ 00 - Fundamentos/desafio.py
@@ -23,6 +23,13 @@ while True:
         if valor > 0:
             saldo += valor
             extrato += f"Depósito: R$ {valor:.2f}\n"
+            # print(f"Depósito de R${valor:.2f} reais confirmado.")
+            print(f'''
+                  ########## Operação #######################
+                  Depósito de R${valor:.2f} reais confirmado
+                  Escolha uma opção do menu abaixo.
+                  ###########################################
+                  ''')
 
         else:
             print("Operação falhou! O valor informado é inválido.")
@@ -49,6 +56,12 @@ while True:
             saldo -= valor
             extrato += f"Saque: R$ {valor:.2f}\n"
             numero_saques += 1
+            print(f'''
+                  ########## Operação #######################
+                  Saque realizado de R${valor:.2f} reais confirmado
+                  Escolha uma opção do menu abaixo.
+                  ###########################################
+                  ''')
 
         else:
             print("Operação falhou! O valor informado é inválido.")
